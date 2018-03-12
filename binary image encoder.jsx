89d151529256095214028c6363f3d6f2a57100b8
Penguino138 <Natelovell99@gmail.com>
2018-03-12 02:03:59 -0600

    Image Testing Files
    Included files you need

---
+++ binary image encoder.jsx
@@ -0,0 +1,17 @@
+﻿var path = '/c/test/';
+var f = File(path+"thumb.png");
+f.encoding = 'BINARY'
+f.open('e');
+
+var binary;
+binary = f.read().toSource();
+
+var myFile = new File("/c/test/binary.txt");
+        myFile.open("w");
+        myFile.encoding = "UTF-8";
+        myFile.write(binary);
+        myFile.close();
+
+$.writeln(binary);
+
+f.close();
